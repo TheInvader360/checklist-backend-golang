e3b9203076cac0584287948a22dfc7c1d87e55ab
Darren <theinvader360@gmail.com>
2020-05-20 12:41:37 +0100

    Deletable tasks and conflict/notfound logic

--- main.go
+++ main.go
@@ -23,10 +23,12 @@ func main() {
 	tasks = append(tasks, task{1001, "Eat", false})
 	tasks = append(tasks, task{1002, "Sleep", false})
 	tasks = append(tasks, task{1003, "Rave", false})
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/task", createTask).Methods("POST")
 	router.HandleFunc("/tasks", readTasks).Methods("GET")
 	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -38,8 +40,20 @@ func createTask(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	json.Unmarshal(reqBody, &reqTask)
-	tasks = append(tasks, reqTask)
-	w.WriteHeader(http.StatusCreated)
+
+	conflict := false
+	for _, task := range tasks {
+		if task.ID == reqTask.ID {
+			conflict = true
+		}
+	}
+
+	if conflict {
+		w.WriteHeader(http.StatusConflict)
+	} else {
+		tasks = append(tasks, reqTask)
+		w.WriteHeader(http.StatusCreated)
+	}
 }
 
 func readTasks(w http.ResponseWriter, r *http.Request) {
@@ -48,21 +62,51 @@ func readTasks(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateTask(w http.ResponseWriter, r *http.Request) {
-	//curl -d '{"id":1002, "info":"Drink", "done":true}' -H "Content-Type: application/json" -i -X PUT http://localhost:8080/tasks/1002
+	//curl -d '{"info":"Drink", "done":true}' -H "Content-Type: application/json" -i -X PUT http://localhost:8080/tasks/1002
 	updateTaskID, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	var reqTask task
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
 	json.Unmarshal(reqBody, &reqTask)
+
+	found := false
+
 	for index, task := range tasks {
 		if task.ID == updateTaskID {
+			found = true
 			tasks[index].Info = reqTask.Info
 			tasks[index].Done = reqTask.Done
 		}
+	}
+
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	//curl -i -X DELETE http://localhost:8080/tasks/1003
+	deleteTaskID, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	found := false
+
+	for index, task := range tasks {
+		if task.ID == deleteTaskID {
+			found = true
+			tasks = append(tasks[:index], tasks[index+1:]...)
+		}
+	}
+
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
 	}
 }
--- readme.md
+++ readme.md
@@ -1,11 +1,14 @@
-### Minimal REST API for the Checklist project
+### Minimal REST API
 
-Terminal 1:
+Run:
 
     cd ~/go/src/github.com/TheInvader360/checklist-backend-golang
     go build
     ./checklist-backend-golang
 
-Terminal 2:
+Usage (CRUD):
 
-    curl http://localhost:8080
+    curl -d '{"id":1004, "info":"Learn", "done":false}' -H "Content-Type: application/json" -i -X POST http://localhost:8080/task
+    curl -i -X GET http://localhost:8080/tasks
+    curl -d '{"info":"Drink", "done":true}' -H "Content-Type: application/json" -i -X PUT http://localhost:8080/tasks/1002
+    curl -i -X DELETE http://localhost:8080/tasks/1003
